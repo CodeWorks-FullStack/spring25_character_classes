0d5cc83f0ebde33df75d84ab9d1f7aafce842ef4
Jeremy Fowler <jembyfowler@gmail.com>
2025-02-24 11:15:06 -0700

    first commit with import examples

---
+++ app/App.js
@@ -0,0 +1,18 @@
+// NOTE import allows us to bring in code from another javascript file
+import { ExampleController } from './controllers/ExampleController.js';
+import whyWouldIDoThis, { name, num } from './ExportExample.js';
+
+const numberFromOtherModule = num
+const nameFromOtherMidule = name
+
+const thisIsWeird = whyWouldIDoThis
+
+class App {
+
+  ExampleController = new ExampleController() // ☑️ you can remove this - example only
+
+}
+
+window['app'] = new App()
+
+
---
+++ app/ExportExample.js
@@ -0,0 +1,7 @@
+export const num = 7
+
+export const name = 'Jermey!'
+
+const weirdThing = 'I am weird'
+
+export default weirdThing
