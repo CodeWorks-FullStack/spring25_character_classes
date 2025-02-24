6801f26c553ad76526a0168f5001712f3a6ab83a
Jeremy Fowler <jembyfowler@gmail.com>
2025-02-24 12:25:03 -0700

    start of our character class

--- app/AppState.js
+++ app/AppState.js
@@ -1,10 +1,15 @@
 import { EventEmitter } from './utils/EventEmitter.js'
 import { createObservableProxy } from './utils/ObservableProxy.js'
 
+// NOTE any member of the ObservableAppState class can be accessed by importing the AppState (declare your global variables here) 🧠
 class ObservableAppState extends EventEmitter {
 
   /**@type {import('./models/Example.js').Example[]} */
   examples = []
+
+  characters = [
+  ]
 }
 
+// step on pedal and car goes
 export const AppState = createObservableProxy(new ObservableAppState())
---
+++ app/models/Character.js
@@ -0,0 +1,12 @@
+
+export class Character {
+  constructor(characterName) {
+    this.name = characterName
+  }
+  name = ''
+  emoji = ''
+  health = 0
+  maxHealth = 0
+  job = ''
+
+}
