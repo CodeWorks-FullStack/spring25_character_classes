572dc91c2bf005e7711f2bd22bfdde7db1eeb9f5
Jeremy Fowler <jembyfowler@gmail.com>
2025-02-24 12:13:42 -0700

    added button with onclick

--- app/App.js
+++ app/App.js
@@ -1,4 +1,5 @@
 // NOTE import allows us to bring in code from another javascript file
+import { CharactersController } from './controllers/CharactersController.js';
 import { ExampleController } from './controllers/ExampleController.js';
 import whyWouldIDoThis, { name, num } from './ExportExample.js';
 
@@ -6,12 +7,14 @@ const numberFromOtherModule = num
 const nameFromOtherMidule = name
 const thisIsWeird = whyWouldIDoThis
 
+
+// NOTE anything added to this class is accessible to the DOM (view)
 class App {
-
   ExampleController = new ExampleController() // ☑️ you can remove this - example only
-
+  coolGuy = "Jake Overall"
+  // the DOM now has access to our class object
+  // NOTE new is a keyword to construct a class (instantiate)
+  charactersController = new CharactersController()
 }
 
 window['app'] = new App()
-
-
--- app/controllers/CharactersController.js
+++ app/controllers/CharactersController.js
@@ -1,7 +1,14 @@
 
+// NOTE the user only has access to what is defined on our Controller classes
 export class CharactersController {
   // NOTE will run when CharactersController class is constructed (happens in App class)
   constructor() {
     console.log('You just created a new Characters Controller!');
+    console.log('Great job buddy!');
+  }
+
+  sayWhatUp() {
+    let name = 'jerms'
+    console.log('What up ' + name);
   }
 }
